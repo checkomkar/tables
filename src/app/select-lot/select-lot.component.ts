993a2988151da6a58d7755f32e90c068492be486
checkomkar <check.omkar@gmail.com>
2017-09-29 15:11:54 +0530

    added modals update/newsite

--- src/app/select-lot/select-lot.component.ts
+++ src/app/select-lot/select-lot.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import {HttpService} from '../services/app.service.httpService';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/modal-options.class';
 
 @Component({
   selector: 'app-select-lot',
@@ -10,8 +12,12 @@ import {HttpService} from '../services/app.service.httpService';
 export class SelectLotComponent implements OnInit {
 	shipments: any;
 	dataUrl = './assets/shipment-list.json';
-  	constructor(private http: HttpService) { }
+	public modalRef: BsModalRef;
+  	constructor(private http: HttpService, private modalService: BsModalService) { }
 
+  	public openModal(template: TemplateRef<any>) {
+		this.modalRef = this.modalService.show(template, {class: 'modal-md'});
+	}
 	ngOnInit() {
 		this.http.getDataObservable(this.dataUrl).subscribe(data => {	        
 	        console.log(data);
